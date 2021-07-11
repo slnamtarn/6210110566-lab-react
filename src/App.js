4974a56935ad2a37fcee54b2962eabbf69364b06
slnamtarn <6210110566@psu.ac.th>
2021-07-11 17:09:57 +0700

    5. Components & States 1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
+import WordCard from './WordCard';
 
 const word = "Hello";
 function App() {
  return (
  <div >
-  {
-      Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
-  }    
+    <WordCard value={word}/>
  </div>
  );
 }
--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,7 +1,15 @@
-import React from 'react';
-export default function CharacterCard(props) {
+import React , {useState} from 'react';
+
+export default function CharacterCard(props){
+    const [active, setActive] = useState(false);
+
+    const activate = () => {
+        setActive(true)
+    }
+
+    const className = `card ${active ? 'activeCard': ''}`
     return (
-        <div className="card">{props.value}</div>
-    ) 
-   }
-   
+        <div className={className} onClick={activate}>{props.value}</div>
+        )
+       
+}
