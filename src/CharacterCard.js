9161f9294a21593f818036018b3c096cd87a12db
slnamtarn <6210110566@psu.ac.th>
2021-07-11 18:04:47 +0700

    5. Handler as Props 2

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -4,8 +4,11 @@ export default function CharacterCard(props){
     const [active, setActive] = useState(false);
 
     const activate = () => {
-        setActive(true)
-        props.activateHandler(props.value)
+        if(!active){
+            setActive(true)
+            props.activateHandler(props.value)
+        }
+           
     }
 
     const className = `card ${active ? 'activeCard': ''}`
