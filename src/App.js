254187d84e5995ef31620e08da996b446c4ea432
slnamtarn <6210110566@psu.ac.th>
2021-07-11 23:23:32 +0700

    part7 3

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import WordCard from './WordCard';
 import { Button } from 'reactstrap';
 
 
-const word = "woranut";
+var word = "woranutdangworawith"
+//const word = "woranut";
 function App() {
  return (
  <div >  
     <Button outline color="success">my name is ... ?</Button>{' '}
-    <WordCard value={word}/>
+    <WordCard value={word} />
+</div>
   
- </div>
+   //const activationHandler = (c) => {
+   //if (c) {
+  //alert("you guessed the correct word !")
+ // window.location.reload();
  );
 }
 export default App;
